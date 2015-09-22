66f06ae9477e36b6d72a22ca2f1fb5377ae26bad
Derek Wider <technoD@outlook.com>
2015-09-21 21:30:57 -0400

    Second round of comments

--- src/org/usfirst/frc1073/SubsystemPID/PIDThread.java
+++ src/org/usfirst/frc1073/SubsystemPID/PIDThread.java
@@ -7,7 +7,7 @@ import org.usfirst.frc1073.SubsystemPID.subsystems.PIDSubsystem;
 	 * @author Derek Wider
 	 * FIRST Team 1073
 	 * 
-	 * A runnable, generic PID class. 
+	 * A runnable, generic PID class. Runs on a separate thread and all system control can be re-routed around a PIDthread should something go wrong. 
 	 *
 	 */
 public class PIDThread implements Runnable{
@@ -33,7 +33,9 @@ public class PIDThread implements Runnable{
 	private boolean enabled;
 	private int marker; //<--- this is SUPER IMPORTANT. Each PID Thread needs its own unique marker, or it will NOT WORK
 	/**
-	 * Constructs a PID Thread
+	 * Constructs a PID Thread. Each PID thread is an implementation of the Runnable interface. PID Threads can be created or stopped anytime, and can also be enabled/disabled ("Paused") when not in use. To use a PID Thread,
+	   you'll need 3 PID-specific objects - 2 PIDSubsystems and a PIDCommand. PIDSubsystem and PIDCommand are interfaces that your subsystems and commands should implement if they are to use PID. If you're looking for more information about how PID works, 
+	   please refer to this introductory piece: http://www.societyofrobots.com/programming_PID.shtml
 	 * @param kP - the Proportional parameter
 	 * @param kI - the integral parameter
 	 * @param kD - the derivative parameter
@@ -56,30 +58,35 @@ public class PIDThread implements Runnable{
 		enabled = true;
 	}
 	/**
-	 * 
+	 * Called when the thread the PIDThread is passed into is started (myThread.start())
 	 */
 	public void run(){
-		
+		//TODO add sensor failure detection to abort loop and return to manual/variable voltage control
 		while (true) { 
+			//skip PID if the thread is disabled
 			if(enabled){
+				//Core PID Code follows: 
+				//setpoint get the setpoint from the PIDSetpoint passed in (use this thread's marker)
+				//currentMeasurement gets the current reading from the PIDinput (use this thread's marker)
 				setpoint = PIDSetpoint.getPIDSetpoint(marker);
 				currentMeasurement = PIDinput.getPIDSource(marker);
-
-				// PID base code below:
+				//PID Calculation (includes tolerance adjustment)
 				double error = (setpoint) - (currentMeasurement);
 				error = toleranceAdjustment(error);
 				integral = integral + (error * dt);
 				double derivative = (error - previousError) / dt;
 				output = (kP * error) + (kI * integral) + (kD * derivative);
-				
 				previousError = error;
-
+				//set the PIDoutput to the generated output (again, use the specific marker to prevent cross-thread data transmission, ex. left front encoder reading used in right front PID)
 				PIDOutput.setPIDOutput(output, marker);
 			}
+			//if PID disabled, just set output to 0 and 0 integral.
+			//IMPORTANT - if switching to manual control, implement a "disregard all data" catch in the setPIDOutput() method of your PIDOutput object to prevent 0 movement from actuator. 
 			else{
 				PIDOutput.setPIDOutput(0.0, marker);
 				integral = 0;
 			}
+			//thread frequency adjustment. catch statement can also be used to trigger events
 			try {
 				Thread.sleep(dt);
 			} catch (InterruptedException e) {
@@ -88,27 +95,41 @@ public class PIDThread implements Runnable{
 		}
 
 	}
-
+	/**
+	* Adjusts the error for the specified tolerance
+	* @param currentError - the current PID error
+	*/
 	private double toleranceAdjustment(double currentError){
 		if(Math.abs(currentError) > tolerance){
 			return currentError;
 		}
 		return 0.0;
 	}
+	/**
+	* Establishes the three PID objects used in the core calculation (each object implements either PIDSubsystem or PIDCommand)
+	* @param PIDinput - the subsystem containing the sensor used in the PID calculation
+	* @param PIDOutput - the subsystem containing the output device (motor controller, relay, etc) used in the PID calculation
+	* @param PIDSetpoint - The command that gets the setpoint used in PID. This can be any command, as long as it specifies a setpoint (can be joystick value, autonomous distance, etc)
+	*/
 	public void setPIDObjects(PIDSubsystem PIDinput, PIDSubsystem PIDOutput, PIDCommand PIDSetpoint){
 		this.PIDinput = PIDinput;
 		this.PIDOutput =  PIDOutput;
 		this.PIDSetpoint = PIDSetpoint;
 	}
-	
+	/**
+	* Disables PID calculations (does NOT stop thread execution)
+	*/
 	public void disable(){
 		enabled = false;
 	}
+	/**
+	* Enables PID calculations (thread is still running, data retrieval simply re-initiated)
+	* Also can be used to reset PID algorithm if integral has wound up
 	public void enable(){
 		enabled = true;
 	}
 	
-	//unused
+	//unused methods but kept in for future reference
 	public void updateSetpoint(double newSetpoint){
 		this.setpoint = newSetpoint;
 	}
--- src/org/usfirst/frc1073/SubsystemPID/subsystems/PIDSubsystem.java
+++ src/org/usfirst/frc1073/SubsystemPID/subsystems/PIDSubsystem.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc1073.SubsystemPID.subsystems;
-
+/**
+* An interface for use with PIDThread. Any subsystem that will be using a PIDThread must implement this interface
+*/
 public interface PIDSubsystem {
 	public double getPIDSource(int marker);
 	public void setPIDOutput(double output, int marker);
